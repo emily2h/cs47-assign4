8b209b06affb0f69713ba58c531317ef539ea7c5
Emily Huang <33561698+emily2h@users.noreply.github.com>
2022-02-03 16:57:58 -0800

    finished assignment 3

---
+++ Themes/colors.js
@@ -0,0 +1,7 @@
+const Colors = {
+  background: "#121212",
+  spotify: "#1DB954",
+  gray: "#B3B3B3",
+};
+
+export default Colors;
---
+++ utils/constants.js
@@ -0,0 +1,13 @@
+export const CLIENT_ID = "e0ea8942335047f1a468698865c4aa38";
+export const SCOPES = [
+  "user-read-currently-playing",
+  "user-read-recently-played",
+  "user-read-playback-state",
+  "user-top-read",
+  "user-modify-playback-state",
+  "streaming",
+  "user-read-email",
+  "user-read-private"
+];
+export const REDIRECT_URI = "exp://10.0.0.249:19000";
+export const ALBUM_ID = "2B87zXm9bOWvAJdkJBTpzF?si=JquL7Oc6RzCuE9uM-3EkRw";
